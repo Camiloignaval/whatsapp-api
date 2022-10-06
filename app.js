4d7e4ffba04ed54c83032ca98c506ef4858aa83b
Camiloignaval <camiloignaval@gmail.com>
2022-10-06 15:40:59 -0300

    fix

--- app.js
+++ app.js
@@ -464,7 +464,7 @@ function startKeepAlive() {
     //   .on("error", function (err) {
     //     console.log("Error: " + err.message);
     //   });
-  }, /* 20 */ 1 * 30 * 1000); // load every 20 minutes
+  }, 20 * 60 * 1000); // load every 20 minutes
 }
 
 startKeepAlive();
