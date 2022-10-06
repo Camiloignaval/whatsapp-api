ccac8344ae743bfddd6d977c0ccce7368a3b602a
Camiloignaval <camiloignaval@gmail.com>
2022-10-06 15:59:22 -0300

    fix

--- app.js
+++ app.js
@@ -436,15 +436,8 @@ function startKeepAlive() {
     try {
       console.log("despertare");
       await fetch("https://whatsapp-api-cv.herokuapp.com");
-      res.status(200).json({
-        status: true,
-      });
     } catch (error) {
       console.log({ error });
-      res.status(400).json({
-        status: false,
-        message: error?.message ?? "",
-      });
     }
     // try {
     // http.get("https://whatsapp-api-cv.herokuapp.com");
